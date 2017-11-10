d3dddf7f0ea9eced8e32ccb02579a19b4472f390
15wtyuan <15wtyuan#stu.edu.cn>
2017-11-10 21:39:22 +0800

    主页可以显示信息了

---
+++ app/src/main/java/com/example/bill/happyenjoy/model/IssueDateJson.java
@@ -0,0 +1,17 @@
+package com.example.bill.happyenjoy.model;
+
+/**
+ * Created by TT on 2017/11/5.
+ */
+
+public class IssueDateJson extends BaseJson {
+    IssueDateList data;
+
+    public IssueDateList getData() {
+        return data;
+    }
+
+    public void setData(IssueDateList data) {
+        this.data = data;
+    }
+}
